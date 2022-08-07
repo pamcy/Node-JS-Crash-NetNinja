76ab74a374fa0a2bc72a22fff354dd611279bdd4
Chinyi Pam <chinyi.pam@gmail.com>
2022-08-07 02:34:33 +0000

    send response page according to the request url

--- basics/05.1-server.js
+++ basics/05.1-server.js
@@ -3,31 +3,36 @@
 const http = require('http')
 const fs = require('fs')
 
-// http.createServer([options][, requestListener])
-// Create a local server to receive data from
 const server = http.createServer((req, res) => {
-    // 1. set the content type to the browser
     res.setHeader('Content-Type', 'text/html')
 
-    // 2. set the content to the browser
-    fs.readFile('basics/views/index.html', (err, data) => {
+    console.log(req)
+
+    let path = 'basics/views/'
+
+    switch (req.url) {
+        case '/':
+            path += 'index.html'
+            break;
+        case '/about':
+            path += 'about.html'
+            break
+        default:
+            path += '404.html'
+    }
+
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.error(err)
             res.end()
         }
 
         res.write(data)
-        
-        // 3. end the response and send it to the browser
         res.end()
     })
 })
 
 
-// Starts the HTTP server listening for connections. 
 server.listen(3000, 'localhost', () => {
-    // the callback fire when the server is listening on the port 3000
-    // sever 啟用時會 log 這行
-
     console.log('listening for requests on port 3000');
 })
